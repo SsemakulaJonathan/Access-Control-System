33ae60cb0c20c680310cc838ed976512b9e52343
SsemakulaJonathan <uhurussemakula069@gmail.com>
2024-11-06 15:12:57 +0800

    Initial commit for Heroku deployment

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Access Control System")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://jonathan-access-control-system.vercel.app/", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
